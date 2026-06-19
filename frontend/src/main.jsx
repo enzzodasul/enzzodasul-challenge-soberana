4fc71fc4de0ea78ff5c9351d7864e152fe1239d1
enzzodasul <enzzodasul@gmail.com>
2026-06-19 05:13:00 -0300

    adição de comtarios no codigo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react'    // É uma ferramenta do React ajuda a detectar: possíveis bugs
+import { createRoot } from 'react-dom/client'   //É a função responsável por criar a aplicação React dentro do HTML.
+import './index.css'  // tá trazendo tudo que ta la no index.css
+import App from './App.jsx'   // importando o coponente principal aqui
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(    // ta conectando na div no incex html 
   <StrictMode>
-    <App />
+    <App />          // Renderize App mas monitore tudo usando StrictMode
   </StrictMode>,
 )
