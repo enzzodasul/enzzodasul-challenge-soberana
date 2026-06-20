194a885725a3861ddb1dc171f419a07790cc4a13
enzzodasul <enzzodasul@gmail.com>
2026-06-19 21:45:06 -0300

    configuração correta do react query agora podendo ser usado em todas as paginas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'    // É uma ferramenta do React ajuda a detectar: possíveis bugs
 import { createRoot } from 'react-dom/client'   //É a função responsável por criar a aplicação React dentro do HTML.
+import { QueryClient,QueryClientProvider } from "@tanstack/react-query"
+
 import './index.css'  // tá trazendo tudo que ta la no index.css
 import App from './App.jsx'   // importando o coponente principal aqui
 
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(    // ta conectando na div no incex html 
   <StrictMode>
-    <App />          
+    <QueryClientProvider client={queryClient}>
+    <App />  
+    </QueryClientProvider>        
   </StrictMode>,
 )
 // Renderize App mas monitore tudo usando StrictMode
